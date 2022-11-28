626fba69fc183749e2aa175becf964278bf300a2
Guillermo Ehrenbolger <68972780+valkanoise@users.noreply.github.com>
2022-11-28 11:55:59 +0100

    Add files via upload

---
+++ Exercises/Chapter 2/ex_2_15_3.py
@@ -0,0 +1,7 @@
+# Gross pay
+inph=input('Hours?\n')
+hrs=float(inph)
+inpr=input('Rate?\n')
+rate=float(inpr)
+pay=hrs*rate
+print('Pay', pay)
---
+++ Exercises/Chapter 2/ex_2_15_5.py
@@ -0,0 +1,5 @@
+#Conversor de Grados Celsius a grados Farenheit
+inptc = input('Grados Celsius a convertir?\n')
+tc=float(inptc)
+tf = tc*(9/5)+32
+print(tf,'grados Farenheit')
